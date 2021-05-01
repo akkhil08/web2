a1f4a8b3f0caed2b4f4d6f8ad3193c9c9eaa28ab
akkhil08 <rakkhilesh@gmail.com>
2021-05-01 17:33:36 +0530

    j

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port=process.env.PORT ||3000;
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/frontend/html/index.html');
 });
-
+app.use(express.static(__dirname+'/frontend'))
 app.listen(port,function(){
    console.log("site running on http://localhost:"+port);
 });
